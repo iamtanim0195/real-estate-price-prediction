f1ebc62b987d894c45939ec4bc5bdfb0b2552df8
iamtanim0195 <sahadatmd195@gmail.com>
2025-08-06 14:55:54 +0600

    updated urls

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,16 +8,23 @@ app = FastAPI()
 # Allow CORS for frontend (replace with your frontend URL)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://real-estate-price-prediction-dun.vercel.app/"],
-    allow_methods=["POST"]
+    allow_origins=[
+        "https://real-estate-price-prediction-dun.vercel.app",  # deployed frontend
+        "http://localhost:3000"  # local development
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 @app.post("/predict")
 def predict(input: PropertyInput):
     """Predict home price based on input features."""
     try:
         # One-hot encode location
-        loc_index = columns.index(input.location.lower()) if input.location.lower() in columns else -1
+        loc_index = columns.index(input.location.lower(
+        )) if input.location.lower() in columns else -1
         x = [0] * len(columns)
         x[0] = input.area
         x[1] = input.bhk
